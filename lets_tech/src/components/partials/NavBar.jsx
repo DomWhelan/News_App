373fbaa5959dfe91c9fa19faaf8f20e15ae9e534
Dominic Whelan <95228980+DomWhelan@users.noreply.github.com>
2023-04-20 15:58:12 -0230

    add dropdown menu

--- lets_tech/src/components/partials/NavBar.jsx
+++ lets_tech/src/components/partials/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faHome } from "@fortawesome/free-solid-svg-icons";
+import Popup from 'reactjs-popup';
 
 export default function NavBar() {
   const homeIcon = <FontAwesomeIcon icon={faHome} />;
@@ -13,9 +14,24 @@ export default function NavBar() {
           {homeIcon}
         </Link>
         <span>{"Lets Tech"}</span>
-        <Link to="" className="headerIcon">
-          {menuIcon}
-        </Link>
+        <Popup 
+        trigger={<div className="headerIcon hamburger"> {menuIcon}</div>} 
+        position="left top"
+        on="hover"
+        closeOnDocumentClick
+        mouseLeaveDelay={300}
+        mouseEnterDelay={5}
+        arrow={false}
+        >
+          <div>
+            <ul className="menuDropdownList">
+              <li className="menuDropdownItem"><Link className="link" to={"/TechNews"}>{"Tech News"}</Link></li>
+              <li className="menuDropdownItem"><Link className="link" to={"/TechNews"}>{"Tech News"}</Link></li>
+              <li className="menuDropdownItem"><Link className="link" to={"/TechNews"}>{"Tech News"}</Link></li>
+              <li className="menuDropdownItem"><Link className="link" to={"/TechNews"}>{"Tech News"}</Link></li>
+            </ul>
+          </div>
+        </Popup>
       </div>
     </div>
   );
